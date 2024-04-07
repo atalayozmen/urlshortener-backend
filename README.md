ce91955006f525963a5d5a44cb4963bf76205c89
atalayozmen <atalayozmen21@gmail.com>
2024-04-07 15:20:19 +0200

    setup for integration tests and readme update

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Backend for URL Shortener
 Simple backend for my URL Shortener project.
+
+Run the project with: `npm run dev`
+
+Testing:
+- Run the test database with `docker-compose up`
+- Migrate database schema with `npm run migrate:test`
+- Run the tests using `npm run test`
--- src/test/get.test.js
+++ src/test/get.test.js
@@ -1,9 +1,30 @@
 const request = require("supertest");
 const app = require("../app");
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+// use `prisma` in your application to read and write data in your DB
+
+
+beforeAll(async () => {
+  // create product categories
+  console.log('prisma', prisma) 
+  await prisma.url.createMany({
+    data: [{ url: 'testurl.com' }, { url: 'testurl2.com' }],
+  })
+
+  console.log('✨ 2 urls successfully created!')
+})
 
 describe("Test the root path", () => {
   test("It should response the GET method", async () => {
     const response = await request(app).get("/urls");
+    console.log(response.body)
     expect(response.statusCode).toBe(200);
   });
 });
+
+afterAll(async () => {
+  await prisma.url.deleteMany()
+  await prisma.$disconnect()
+})
